4179ecdaa5bab9953bb32a2995045aa855b7d744
eskokado <eskokado@gmail.com>
2023-04-06 10:43:47 -0300

    feat: create Supplier model

---
+++ src/main/java/com/anjun/eskokado/apideliveryesk/domain/models/Supplier.java
@@ -0,0 +1,23 @@
+package com.anjun.eskokado.apideliveryesk.domain.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "suppliers")
+@Data
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String number;
+
+    @Column(length = 100)
+    private String complement;
+
+    @Column(name = "zip_code", length = 8, nullable = false)
+    private String zipCode;
+}
