f215312cba07f8fe587877ef8cddf69ff22418b2
eskokado <eskokado@gmail.com>
2023-04-07 07:31:41 -0300

    feat: create custom response of validation fields dto

---
+++ src/main/java/com/anjun/eskokado/apideliveryesk/resources/dto/FieldError.java
@@ -0,0 +1,28 @@
+package com.anjun.eskokado.apideliveryesk.resources.dto;
+
+public class FieldError {
+
+    private String field;
+    private String message;
+
+    public FieldError(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
