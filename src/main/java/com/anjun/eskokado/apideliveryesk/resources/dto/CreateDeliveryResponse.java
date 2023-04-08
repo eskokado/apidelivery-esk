49a0dba131878db1ef0c479171818b2891cc8a71
eskokado <eskokado@gmail.com>
2023-04-08 06:04:03 -0300

    refactor: clear unused code

--- src/main/java/com/anjun/eskokado/apideliveryesk/resources/dto/CreateDeliveryResponse.java
+++ src/main/java/com/anjun/eskokado/apideliveryesk/resources/dto/CreateDeliveryResponse.java
@@ -10,13 +10,6 @@ public class CreateDeliveryResponse {
     private Order order;
     private List<OrderItem> orderItems;
     private List<ResponseError> errors;
+    private ZipCodeAddress zipCodeAddress;
     private int statusCode;
-
-    public void setErrors(List<ResponseError> errors) {
-        this.errors = errors;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
 }
