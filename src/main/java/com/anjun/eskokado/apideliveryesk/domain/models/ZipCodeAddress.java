f2b491a5c91fc37385fabfe7080e6109631e4922
eskokado <eskokado@gmail.com>
2023-04-08 06:07:57 -0300

    feat: create ZipCodeAddress model

---
+++ src/main/java/com/anjun/eskokado/apideliveryesk/domain/models/ZipCodeAddress.java
@@ -0,0 +1,17 @@
+package com.anjun.eskokado.apideliveryesk.domain.models;
+
+import lombok.Data;
+
+@Data
+public class ZipCodeAddress {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
